33a13dd1e8ffd70c8fca2c58805934d7bb916436
Billy Kaplan <billy1kaplan@gmail.com>
2017-07-28 11:20:14 -0700

    .

--- ex1_28.scm
+++ ex1_28.scm
@@ -1,45 +1,26 @@
-( define ( timed-prime-test3 n )
-         (newline)
-         (display n)
-         (start-prime-test (oddstart n) 30 (runtime)))
-
-(define (start-prime-test n count start-time)
-  ( cond (( = count 0 ) (report-prime n ( - (runtime) start-time)))
-         ((fast-prime?  n 10) (start-prime-test ( + n 2 ) ( - count 1 ) start-time ))
-         ( else ( start-prime-test ( + n 2 ) count start-time ))))
-
-(define (report-prime n elapsed-time)
-  (newline)
-  (display n )
-  (newline)
-  elapsed-time)
+;copied, need to revisit
 
 ( define ( square n )
          ( * n n))
 
-( define ( expmod base exp m )
-         ( cond (( = exp 0 ) 1)
-                (( even? exp )
-                 ( remainder
-                   ( square (expmod base ( / exp 2 ) m ))
-                   m))
-                 ( else
-                   ( remainder
-                     ( * base ( expmod base ( - exp 1 ) m ))
-                     m))))
+( define ( miller-rabin n )
+         ( miller-rabin-test ( - n 1 ) n ))
 
-( define ( fermet-test n )
-         ( define ( try-it a )
-                  ( = ( expmod a n n ) a ))
-         ( try-it ( + 1 ( random ( - n 1 )))))
-
-( define ( fast-prime? n times )
-         ( cond (( = times 0 ) true )
-                (( fermet-test n )
-                 ( fast-prime? n ( - times 1 )))
+( define ( miller-rabin-test a n )
+         ( cond (( = a 0 ) #t )
+                (( = ( expmod a ( - n 1 ) n ) 1) miller-rabin-test ( - a 1 ) n )
                 ( else #f )))
 
-( define ( oddstart n )
-         ( if ( even? n ) ( + 1 n )
-              n))
+( define ( expmod base exp  m )
+         ( cond (( = exp 0 ) 1)
+                (( even? exp )
+                 ( let (( x ( expmod base ( / exp 2 ) m )))
+                       ( if ( non-trivial-sqrt? x m )
+                            0
+                            ( remainder ( square x ) m ))))
+                ( else ( remainder ( * base ( expmod ( base ( - exp 1 ) m ))) m))))
 
+( define ( non-trivial-sqrt? n m )
+         ( cond (( = n 1 ) #f )
+                (( = n ( - m 1 ) #f ))
+                ( else ( = ( remainder ( square n ) m ) 1 ))))
